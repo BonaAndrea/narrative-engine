4545860f3a1d719aa59978829b0ca952de17c182
Andrea Bona <andrea.bona1996@gmail.com>
2026-05-10 22:17:57 +0200

    Disable HTTPS redirection in production

--- NarrativeEngine.API/Program.cs
+++ NarrativeEngine.API/Program.cs
@@ -35,8 +35,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseCors("AllowAny");
 
 app.MapControllers();
